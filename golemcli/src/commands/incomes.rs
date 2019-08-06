e2ad65d226f51b7650f8522dde58ab9078411545
Przemysław K. Rekucki <przemyslaw.rekucki@golem.network>
2019-08-06 13:28:33 +0200

    reformat for incomes

--- golemcli/src/commands/incomes.rs
+++ golemcli/src/commands/incomes.rs
@@ -1,38 +1,46 @@
 use crate::context::*;
+use bigdecimal::BigDecimal;
 use futures::prelude::*;
-use golem_rpc_api::pay::{AsGolemPay, Income};
+use golem_rpc_api::pay::{AsGolemPay, Income, PaymentStatus};
+use std::collections::BTreeMap;
 use structopt::{clap::arg_enum, StructOpt};
 
 const INCOMES_COLUMNS: &[&str] = &["payer", "status", "value"];
 
 #[derive(StructOpt, Debug)]
-pub enum Section {
-    /// Show incomes
-    #[structopt(name = "show")]
-    Show {
-        /// Filter by status
-        #[structopt(long = "filter")]
-        #[structopt(
-            parse(try_from_str),
-            raw(
-                possible_values = "&[\"awaiting\",\"confirmed\"]",
-                case_insensitive = "true"
-            )
-        )]
-        filter_by: Option<crate::eth::PaymentStatus>,
+pub struct Section {
+    /// Sort payments
+    #[structopt(long = "sort")]
+    #[structopt(
+        parse(try_from_str),
+        raw(possible_values = "INCOMES_COLUMNS", case_insensitive = "true")
+    )]
+    sort_by: Option<String>,
+    #[structopt(long)]
+    full: bool,
 
-        #[structopt(long = "sort")]
-        #[structopt(
-            parse(try_from_str),
-            raw(possible_values = "INCOMES_COLUMNS", case_insensitive = "true")
-        )]
-        /// Sort incomes
-        sort_by: Option<String>,
+    #[structopt(subcommand)]
+    command: SectionCommand,
+}
 
-        /// Show full table contents
-        #[structopt(long)]
-        full: bool,
-    },
+#[derive(StructOpt, Debug)]
+enum SectionCommand {
+    #[structopt(name = "all")]
+    All,
+    #[structopt(name = "awaiting")]
+    Awaiting,
+    #[structopt(name = "confirmed")]
+    Confirmed,
+}
+
+impl SectionCommand {
+    fn to_payment_status(&self) -> Option<crate::eth::PaymentStatus> {
+        match self {
+            SectionCommand::All => None,
+            SectionCommand::Awaiting => Some(crate::eth::PaymentStatus::Awaiting),
+            SectionCommand::Confirmed => Some(crate::eth::PaymentStatus::Confirmed),
+        }
+    }
 }
 
 arg_enum! {
@@ -49,13 +57,12 @@ impl Section {
         &self,
         endpoint: impl actix_wamp::RpcEndpoint + Clone + 'static,
     ) -> Box<dyn Future<Item = CommandResponse, Error = Error> + 'static> {
-        match self {
-            Section::Show {
-                filter_by,
-                sort_by,
-                full,
-            } => Box::new(self.show(endpoint, filter_by, sort_by, full)),
-        }
+        Box::new(self.show(
+            endpoint,
+            &self.command.to_payment_status(),
+            &self.sort_by,
+            &self.full,
+        ))
     }
 
     pub fn show(
@@ -74,6 +81,9 @@ impl Section {
             .get_incomes_list()
             .from_err()
             .and_then(move |incomes| {
+                let mut total_value = BigDecimal::from(0u32);
+                let mut total_for_status: BTreeMap<PaymentStatus, BigDecimal> = BTreeMap::new();
+
                 let columns = INCOMES_COLUMNS.iter().map(|&name| name.into()).collect();
                 let values = incomes
                     .into_iter()
@@ -87,6 +97,14 @@ impl Section {
                             false => crate::eth::public_to_addres(income.payer),
                             true => income.payer,
                         };
+
+                        total_value += &income.value;
+                        if let Some(total) = total_for_status.get_mut(&income.status) {
+                            *total = total.clone() + income.value.clone();
+                        } else {
+                            total_for_status.insert(income.status.clone(), income.value.clone());
+                        }
+
                         let status = income.status;
                         let value = crate::eth::Currency::GNT.format_decimal(&income.value);
 
@@ -94,7 +112,23 @@ impl Section {
                     })
                     .collect();
 
-                Ok(ResponseTable { columns, values }.sort_by(&sort_by).into())
+                let mut summary = Vec::new();
+                for (k, v) in total_for_status {
+                    summary.push(serde_json::json!([
+                        "",
+                        k,
+                        crate::eth::Currency::GNT.format_decimal(&v)
+                    ]));
+                }
+                summary.push(serde_json::json!([
+                    "",
+                    "",
+                    crate::eth::Currency::GNT.format_decimal(&total_value)
+                ]));
+
+                Ok(ResponseTable { columns, values }
+                    .sort_by(&sort_by)
+                    .with_summary(summary))
             })
     }
 }
